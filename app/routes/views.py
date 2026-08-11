de31aa851069b59b287ffbab7560f718b23e2391
AntoniouKonstantinos <antoniou.kostas97@gmail.com>
2026-08-11 14:05:18 +0300

    Add files via upload

--- app/routes/views.py
+++ app/routes/views.py
@@ -1,3 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-bp = Blueprint('views', __name__)
+bp = Blueprint("views", __name__)
+
+
+@bp.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@bp.route("/employees")
+def employees_page():
+    return render_template("employees.html")
+
+
+@bp.route("/time-off")
+def time_off_page():
+    return render_template("time_off.html")
